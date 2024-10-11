71a9f9f8994195f73f0210950a11e8d289afcf6f
Leon Montealegre <leonm99@gmail.com>
2024-10-11 18:21:35 -0400

    Added more cuisines

--- src/model/schema/Cuisine.ts
+++ src/model/schema/Cuisine.ts
@@ -5,6 +5,17 @@ export const CUISINES = [
     "Indian",
     "Japanese",
     "Viatnamese",
+    "Italian",
+    "Thai",
+    "Mexican",
+
+    "Coffee",
+    "Soup",
+    "Smoothie",
+    "Fast Food",
+    "Pizza",
+    "Asian",
+    "Sandwiches",
 ] as const;
 
 export type Cuisine = typeof CUISINES[number];
